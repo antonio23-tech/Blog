48df83ea988fea401e38feb60bd1fb41df1fbba4
antonio <antonio.tics.23@gmail.com>
2021-01-22 19:42:05 -0600

    Menu funcionando con tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<x-app-layout>
+    <h1>holaaaaa</h1>
+</x-app-layout>
+
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +134,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
